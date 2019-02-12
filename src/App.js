b4313fa00986cc4ae9a1ccc2fc7a8a63c7908c38
GuiFSs <devguisantos@gmail.com>
2019-02-12 01:52:07 -0200

    add Carousel component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { ThemeProvider } from 'react-native-elements';
 import MHeader from './componetns/Header/MHeader';
 import Test from './componetns/Testing/Test';
+import Carousel from './Carousel/Carousel';
 
 const theme = {
   Header: {
@@ -15,7 +16,7 @@ class App extends Component {
     return (
       <ThemeProvider theme={theme}>
         <MHeader />
-        <Test />
+        {/* <Test /> */}
       </ThemeProvider>
     );
   }
