8b10bf89c79f47e88500db1641bcc3f973a59db5
AndersonSantAnna <anderson.santanna14@gmail.com>
2019-02-13 09:50:09 -0200

    carousel

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { ThemeProvider } from 'react-native-elements';
 import MHeader from './componetns/Header/MHeader';
 import Test from './componetns/Testing/Test';
-
+import Carousel from './componetns/Carousel/Carousel'
 const theme = {
   Header: {
     backgroundColor: '#E60014'
@@ -15,7 +15,7 @@ class App extends Component {
     return (
       <ThemeProvider theme={theme}>
         <MHeader />
-        <Test />
+        <Carousel />
       </ThemeProvider>
     );
   }
