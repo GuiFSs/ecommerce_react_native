c70db263a8cd4375e605a2aa02eba2f8c5c9ccb3
GuiFSs <devguisantos@gmail.com>
2019-02-17 23:56:28 -0300

    add items to drawer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { createDrawerNavigator, createAppContainer } from 'react-navigation';
 
 import HomeScreen from './screens/HomeScreen';
+import DrawerList from './components/Drawer/DrawerList';
+import Departamentos from './screens/Departamentos';
 
-const App = createDrawerNavigator({
-  Home: {
-    screen: HomeScreen
-  }
-});
+const App = createDrawerNavigator(
+  {
+    home: {
+      screen: HomeScreen
+    },
+    departamentos: {
+      screen: Departamentos
+    }
+  },
+
+  { contentComponent: DrawerList }
+);
 
 export default createAppContainer(App);
---
+++ src/components/Drawer/DrawerList.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { ScrollView, View, StyleSheet } from 'react-native';
+
+import { ListItem, Icon, Text } from 'react-native-elements';
+
+import { NavigationActions } from 'react-navigation';
+
+export default class DrawerList extends Component {
+  state = {
+    showLoginModal: false,
+    list: [
+      {
+        title: 'destaques',
+        icon: 'home',
+        routeName: 'home'
+      },
+      {
+        title: 'departamentos',
+        icon: 'list-ul',
+        routeName: 'departamentos'
+      }
+    ]
+  };
+  onPressItem = route => {
+    const navigateAction = NavigationActions.navigate({
+      routeName: route
+    });
+    this.props.navigation.dispatch(navigateAction);
+  };
+
+  handlerLoginModal = () => {
+    this.setState(prevState => ({
+      showLoginModal: !prevState.showLoginModal
+    }));
+  };
+
+  render() {
+    const { list, showLoginModal } = this.state;
+    console.log(showLoginModal);
+
+    if (showLoginModal) {
+      //TODO: fazer um modal para login e cadastro
+      console.log('mostrar modal');
+    }
+
+    return (
+      <View onTouchEnd={this.handlerLoginModal}>
+        <View style={styles.userView}>
+          <Icon
+            name='person'
+            type='material'
+            color='#DADADA'
+            size={60}
+            containerStyle={{ left: 20, top: 10 }}
+          />
+          <View>
+            <Text style={{ ...styles.textUser, ...styles.textLogin }}>
+              Faça seu login
+            </Text>
+
+            <Text style={{ ...styles.textUser, ...styles.textCadastro }}>
+              ou cadastre-se.
+            </Text>
+          </View>
+        </View>
+        <ScrollView>
+          <View>
+            {list.map((item, i) => (
+              <ListItem
+                key={i}
+                title={item.title}
+                leftIcon={{ name: item.icon, type: 'font-awesome' }}
+                onPress={() => this.onPressItem(item.routeName)}
+                titleStyle={{ color: '#000' }}
+              />
+            ))}
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  userView: {
+    backgroundColor: '#E60014',
+    height: 80,
+    flexDirection: 'row'
+  },
+  textUser: {
+    top: 10,
+    left: 30
+  },
+  textLogin: {
+    color: '#fff',
+    fontSize: 22
+  },
+  textCadastro: {
+    color: '#DADADA',
+    fontSize: 16
+  }
+});
